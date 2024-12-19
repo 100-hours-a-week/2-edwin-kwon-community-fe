2264d1a2f67ac3c60d4bf7ec26e5c3100801c602
gkwon <gkwon@student.42seoul.kr>
2024-12-19 14:25:08 +0900

    [fix]: fixed dotenv error

--- src/env.js
+++ src/env.js
@@ -1,3 +1,7 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const apiVersion = 'v1';
 const API_BASE_URL = `http://${process.env.HOST}:8000/api/${apiVersion}`;
 const PUBLIC_URL = `http://${process.env.HOST}:8000/public`;
